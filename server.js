f4944cd6b40d5eca7187d0b1cf03cd555f91eeb3
Rajat agrawal <rajat.agrawal193@gmail.com>
2020-07-25 05:14:19 +0530

    Deployement- port edited and procfile added

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express = require('express');
-const bodyParser = require('body-parser');  
-const path = require ('path');
+const bodyParser = require('body-parser');
+const path = require('path');
 const api = require('./server/routes/api');
 
 const port = 3000;
 
-const app = express(); 
+const app = express();
 
-app.use(express.static(path.join(__dirname,'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 app.use('/api', api);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'))}
-    );
-app.listen( port, function(){
-    console.log('server running on localhost'+ port);
+    res.sendFile(path.join(__dirname, 'dist/index.html'))
+});
+app.listen(process.env.PORT || 3000, function () {
+    console.log('server running on localhost' + port);
 })
